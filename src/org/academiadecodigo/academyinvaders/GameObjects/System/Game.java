b4be93621500105dc0db7b279fe17f0fb6ac55f9
rikkarth <ricardominamendes@outlook.com>
2022-10-11 00:22:21 +0100

    added coments to class Game

--- src/org/academiadecodigo/academyinvaders/GameObjects/System/Game.java
+++ src/org/academiadecodigo/academyinvaders/GameObjects/System/Game.java
@@ -5,18 +5,32 @@ import org.academiadecodigo.academyinvaders.GameObjects.Enemy.EnemyForm.*;
 import org.academiadecodigo.academyinvaders.GameObjects.Grid.*;
 import org.academiadecodigo.academyinvaders.GameObjects.Player.Player;
 
+/**
+ * The game logic
+ */
 public class Game {
 
     //PARAMETERS
+
+    /**
+     * Graphical Grid
+     */
     private final Grid GAME_GRID;
     private Player player;
 
+    /**
+     * Container of Enemies
+     */
     private Enemy enemy;
 
+    /**
+     * Animation delay
+     */
     private final int DELAY;
 
     /**
      * Constructs a new game
+     *
      * @param delay animation delay
      */
     public Game(int delay) {
@@ -24,20 +38,37 @@ public class Game {
         this.DELAY = delay;
     }
 
+    /**
+     * Initializes Game Grid
+     * Creates Enemies for the level
+     */
     public void init() {
 
         GAME_GRID.init();
 
-        enemy = new Baljeet(GAME_GRID.makeGridPosition(500,50));
+        enemy = new Baljeet(GAME_GRID.makeGridPosition(500, 50));
 
         enemy.setGrid(GAME_GRID);
 
         player = new Player(GAME_GRID.makeGridPosition(500, 500));
     }
 
-    public void start() {
+    /**
+     * Starts the game
+     *
+     * @throws InterruptedException
+     */
+    public void start() throws InterruptedException {
 
+        while (true) {
+
+            // Pause for a while
+            Thread.sleep(DELAY);
+
+            /**
+             * Insert Game Loop here
+             */
+        }
     }
-
-
 }
+
